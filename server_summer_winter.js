e14ddca8b5b8e7295fa1c36db773bf923faedc16
gd <gd@gdx>
2017-08-12 15:26:21 +0530

    added my sql interface. Now code works for queries of min, max and average type. Note one must have mysql server running with a database created using queries from a file generated online using iris.csv dataset.

--- server_summer_winter.js
+++ server_summer_winter.js
@@ -3,25 +3,65 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "gaurah12"
+});
+
+con.connect(function(err) {
+  if (err)
+    throw new Error("Oops something happened");
+  console.log("Connected!");
+});
+
+con.query('use IOT;', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+});
+
+
+
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var query_str = q.path;
   //console.log(str);
   var query_parsed_str = parse_query(query_str);
   console.log(query_parsed_str);
-  //console.log(query_parsed_str);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data)
-    {
-    if (err)
-     {
+  var mysql_query = 'select '+query_parsed_str[0]+'('+query_parsed_str[1]+') from flower where iris=\''+query_parsed_str[2]+'\';';
+  console.log(mysql_query); 
+  //con.query('select avg(sepal_width) from flower where iris=\'Iris-versicolor\';', function(err, rows, fields) {
+  con.query(mysql_query, function(err, rows, fields)
+  {
+    if (!err)
+      {
+      console.log(rows);
+      
+      }
+    else
+      {
+      console.log('Error while performing Query.');
       res.writeHead(404, {'Content-Type': 'text/html'});
-       return res.end("404 Not Found");
-     }  
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
+      return res.end("404 Not Found");
+      }
+  
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      //var pair = rows.split(': ');
+      for (var i in rows){
+        var a = rows[i];
+        console.log(a);
+      }
+      res.write(JSON.stringify(rows[0])+' from '+query_parsed_str[2]); //convert rows object to a string and send to server
+
+
+      return res.end();
   });
+  
 }).listen(8080);
 
 function parse_query(url_str)
@@ -53,10 +93,7 @@ function parse_query(url_str)
         break;
         default:
         console.log('query incorrect');
-         
       }
-
-
   }
   //console.log(query_parsed_str);
   return query_parsed_str;
